17387d5f278d8162af73565f6613b2758c4a5109
Mihail Sokolskiy <mihail.sokolskiy@gmail.com>
2025-03-25 21:11:01 +0300

    ======= develop === ДАТА 25-03-25 === ВРЕМЯ 21:11:01 =======

--- MAKEFILES/git.mk
+++ MAKEFILES/git.mk
@@ -41,7 +41,7 @@ add-remote-repo:
 	-git remote add origin $(GITLABPROJECTURL)
 # наконец добавим ссылки на push в гитхаб и в придётся ещё раз сделать для гитлаб 
 	git remote set-url --add --push origin git@github.com:mysokolsky/$(GITHUBPROJECTNAME).git
-	# git remote set-url --add --push origin $(GITLABPROJECTURL)
+	git remote set-url --add --push origin $(GITLABPROJECTURL)
 # выведем на экран для контроля, куда привязали
 	git remote -v
 
