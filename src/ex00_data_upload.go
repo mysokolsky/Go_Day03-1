fe81c3b00568a07133757c1659ef981c3bed98c9
Jonella Buratto <jonellab@ga-e5.msk.21-school.ru>
2025-04-04 23:33:31 +0300

    ======= develop === ДАТА 04-04-25 === ВРЕМЯ 23:33:30 =======

--- src/ex00_data_upload.go
+++ src/ex00_data_upload.go
@@ -23,23 +23,21 @@
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
+	// "bytes"
+	// "crypto/tls"
+	// "fmt"
+	// "io"
+	// "log"
+	// "net/http"
 	"os"
 	"runtime"
-
 	// сначала нужно перейти в корневую папку Go_Day03-1
 	// потом поскольку там нет файла go.mod + go.sum, создать их командой go mod init $(basename $(PWD)) или запустить цель make gomodinit
 	// потом установить библиотеку go get github.com/elastic/go-elasticsearch/v8 или запустить цель make gogetelastic
-	"github.com/elastic/go-elasticsearch/v8"
-
+	// "github.com/elastic/go-elasticsearch/v8"
 	// сначала нужно закачать библиотеку командой go get github.com/gocarina/gocsv
-	"encoding/csv"
-	"github.com/gocarina/gocsv"
+	// "encoding/csv"
+	// "github.com/gocarina/gocsv"
 )
 
 // ID
@@ -50,15 +48,23 @@ import (
 // Latitude
 
 type Location struct {
-	Longitude float64 `csv:"Longitude" json:"lon"`
-	Latitude  float64 `csv:"Latitude" json:"lat"`
+	Longitude float64 `json:"lon"`
+	Latitude  float64 `json:"lat"`
 }
 
 type Restaurants struct {
-	Name     string   `csv:"Name" json:"name"`
-	Address  string   `csv:"Address" json:"address"`
-	Phone    string   `csv:"Phone" json:"phone"`
+	Name     string   `json:"name"`
+	Address  string   `json:"address"`
+	Phone    string   `json:"phone"`
 	Location Location `json:"location"`
+}
+
+type RestaurantsCSV struct {
+	Name      string  `csv:"Name"`
+	Address   string  `csv:"Address"`
+	Phone     string  `csv:"Phone"`
+	Longitude float64 `csv:"Longitude"`
+	Latitude  float64 `csv:"Latitude"`
 }
 
 func getElasticPassword() string {
@@ -70,75 +76,87 @@ func getElasticPassword() string {
 	return string(data)
 }
 
-func main() {
-	// Создаем клиент Elasticsearch с использованием HTTPS и аутентификации
-	es, err := elasticsearch.NewClient(elasticsearch.Config{
-		Addresses: []string{
-			"https://localhost:9200", // Ваш сервер Elasticsearch
+func (r RestaurantsCSV) ToRestaurants() Restaurants {
+	return Restaurants{
+		Name:    r.Name,
+		Address: r.Address,
+		Phone:   r.Phone,
+		Location: Location{
+			Longitude: r.Longitude,
+			Latitude:  r.Latitude,
 		},
-		Username: "elastic",            // Имя пользователя
-		Password: getElasticPassword(), // Ваш пароль
-
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true, // Отключаем проверку сертификата (для разработки)
-			},
-		},
-	})
-	if err != nil {
-		log.Fatalf("Ошибка при создании клиента: %v", err)
 	}
-
-	// Проверяем соединение с сервером
-	res, err := es.Info()
-	if err != nil {
-		log.Fatalf("Ошибка при подключении к Elasticsearch: %v", err)
-	}
-	defer res.Body.Close()
-
-	// Читаем тело ответа
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatalf("Ошибка чтения ответа: %v", err)
-	}
-
-	// Выводим ответ
-	fmt.Println("Ответ от сервера:", string(body))
-
-	// Определяем схему индекса
-	mapping := `{
-		"settings": {
-			"number_of_shards": 1,
-			"number_of_replicas": 1
-		},
-		"mappings": {
-			"properties": {
-				"name": {"type": "text"},
-				"address": {"type": "text"},
-				"phone": {"type": "text"},
-				"location": {"type": "geo_point"}
-			}
-		}
-	}`
-
-	// Создание индекса "places"
-	res, err = es.Indices.Create("places", es.Indices.Create.WithBody(bytes.NewReader([]byte(mapping))))
-	if err != nil {
-		log.Fatalf("Ошибка создания индекса: %s", err)
-	}
-	defer res.Body.Close()
-
-	// Читаем тело ответа
-	body, err = io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatalf("Ошибка чтения ответа: %s", err)
-	}
-
-	// Проверяем код ответа
-	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusCreated {
-		log.Fatalf("Ошибка при создании индекса: %s\nОтвет: %s", res.Status(), string(body))
-	}
-
-	fmt.Println("✅ Индекс 'places' успешно создан!")
-	fmt.Println("Ответ от Elasticsearch:", string(body))
 }
+
+// func main() {
+// 	// Создаем клиент Elasticsearch с использованием HTTPS и аутентификации
+// 	es, err := elasticsearch.NewClient(elasticsearch.Config{
+// 		Addresses: []string{
+// 			"https://localhost:9200", // Ваш сервер Elasticsearch
+// 		},
+// 		Username: "elastic",            // Имя пользователя
+// 		Password: getElasticPassword(), // Ваш пароль
+
+// 		Transport: &http.Transport{
+// 			TLSClientConfig: &tls.Config{
+// 				InsecureSkipVerify: true, // Отключаем проверку сертификата (для разработки)
+// 			},
+// 		},
+// 	})
+// 	if err != nil {
+// 		log.Fatalf("Ошибка при создании клиента: %v", err)
+// 	}
+
+// 	// Проверяем соединение с сервером
+// 	res, err := es.Info()
+// 	if err != nil {
+// 		log.Fatalf("Ошибка при подключении к Elasticsearch: %v", err)
+// 	}
+// 	defer res.Body.Close()
+
+// 	// Читаем тело ответа
+// 	body, err := io.ReadAll(res.Body)
+// 	if err != nil {
+// 		log.Fatalf("Ошибка чтения ответа: %v", err)
+// 	}
+
+// 	// Выводим ответ
+// 	fmt.Println("Ответ от сервера:", string(body))
+
+// 	// Определяем схему индекса
+// 	mapping := `{
+// 		"settings": {
+// 			"number_of_shards": 1,
+// 			"number_of_replicas": 1
+// 		},
+// 		"mappings": {
+// 			"properties": {
+// 				"name": {"type": "text"},
+// 				"address": {"type": "text"},
+// 				"phone": {"type": "text"},
+// 				"location": {"type": "geo_point"}
+// 			}
+// 		}
+// 	}`
+
+// 	// Создание индекса "places"
+// 	res, err = es.Indices.Create("places", es.Indices.Create.WithBody(bytes.NewReader([]byte(mapping))))
+// 	if err != nil {
+// 		log.Fatalf("Ошибка создания индекса: %s", err)
+// 	}
+// 	defer res.Body.Close()
+
+// 	// Читаем тело ответа
+// 	body, err = io.ReadAll(res.Body)
+// 	if err != nil {
+// 		log.Fatalf("Ошибка чтения ответа: %s", err)
+// 	}
+
+// 	// Проверяем код ответа
+// 	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusCreated {
+// 		log.Fatalf("Ошибка при создании индекса: %s\nОтвет: %s", res.Status(), string(body))
+// 	}
+
+// 	fmt.Println("✅ Индекс 'places' успешно создан!")
+// 	fmt.Println("Ответ от Elasticsearch:", string(body))
+// }
--- src/example_csv_reader.go
+++ src/example_csv_reader.go
@@ -10,21 +10,23 @@ import (
 	"github.com/gocarina/gocsv"
 )
 
-type Industry struct {
-	CompanyName                string `csv:"Organization Name"`
-	LinkedIn                   string `csv:"LinkedIn"`
-	Website                    string `csv:"Website"`
-	TotalFundingAmount         int    `csv:"Total Funding Amount"`
-	TotalFundingAmountCurrency string `csv:"Total Funding Amount Currency"`
-	HeadquartersLocation       string `csv:"Headquarters Location"`
-}
+// type Industry struct {
+// 	CompanyName                string `csv:"Organization Name"`
+// 	LinkedIn                   string `csv:"LinkedIn"`
+// 	Website                    string `csv:"Website"`
+// 	TotalFundingAmount         int    `csv:"Total Funding Amount"`
+// 	TotalFundingAmountCurrency string `csv:"Total Funding Amount Currency"`
+// 	HeadquartersLocation       string `csv:"Headquarters Location"`
+// }
 
 // 5.57ms -> 600 records (read)
 func main() {
 	now := time.Now()
-	readChannel := make(chan Industry, 1)
+	// readChannel := make(chan Industry, 1)
+	readChannel := make(chan RestaurantsCSV, 25)
 
-	readFilePath := "process.csv"
+	// readFilePath := "process.csv"
+	readFilePath := "../materials/data.csv"
 
 	// Open the CSV readFile
 	readFile, err := os.OpenFile(readFilePath, os.O_RDONLY, os.ModePerm)
@@ -49,20 +51,21 @@ func main() {
 	fmt.Println(time.Since(now), count)
 }
 
-func readFromCSV(file *os.File, c chan Industry) {
-	gocsv.SetCSVReader(func(r io.Reader) gocsv.CSVReader {
-		reader := csv.NewReader(r)
-		reader.Comma = ','
-		reader.LazyQuotes = true
-		reader.FieldsPerRecord = -1
-		return reader
-	})
+func readFromCSV(file *os.File, c chan RestaurantsCSV) {
 
-	// Read the CSV file into a slice of Record structs
+	// Создаём новый CSV-ридер с настроенными параметрами
+	reader := csv.NewReader(file)
+	reader.Comma = '\t'         // Указываем разделитель (табуляция)
+	reader.LazyQuotes = true    // Разрешаем необычные кавычки
+	reader.FieldsPerRecord = -1 // Не проверяем количество полей
+
+	// Запускаем асинхронное чтение и отправку в канал
 	go func() {
+		// Пытаемся распарсить весь файл в канал
 		err := gocsv.UnmarshalToChan(file, c)
 		if err != nil {
-			panic(err)
+			fmt.Println("Ошибка при чтении CSV:", err)
+			close(c) // Закрываем канал, если произошла ошибка
 		}
 	}()
 }
