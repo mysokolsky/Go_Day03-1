0ef97cd4441433855ea9654e49e9b41abc3cf5ae
Jonella Buratto <jonellab@ox-s6.msk.21-school.ru>
2025-05-23 21:56:13 +0300

    ======= develop === ДАТА 23-05-25 === ВРЕМЯ 21:56:13 =======

--- src/ex01_getplaces.go
+++ src/ex01_getplaces.go
@@ -8,3 +8,12 @@ package main
 // func (r Restaurants) GetPlaces(limit int, offset int) ([]types.Place, int, error) {
 
 // }
+
+
+// 1. Получать ?page=N через r.URL.Query().Get("page").
+
+// 2. Конвертировать его в int.
+
+// 3. Вычислять, с какой страницы начинать, и передавать в search_after.
+
+// 4. Полученные записи из elastic передавать в Page и рендерить шаблон.
