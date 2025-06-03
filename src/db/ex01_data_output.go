61568da63c7b27a4add3b43c4608fa2519772e9b
Jonella Buratto <jonellab@at-m5.msk.21-school.ru>
2025-06-03 21:18:16 +0300

    ======= develop === ДАТА 03-06-25 === ВРЕМЯ 21:18:16 =======

--- src/db/ex01_data_output.go
+++ src/db/ex01_data_output.go
@@ -14,4 +14,17 @@ import "Go_Day03-1/src/types"
 type Store interface {
 	// возвращает список записей, общее количество найденных записей и (или) ошибку
 	GetPlaces(limit int, offset int) ([]types.Place, int, error)
+
+	// принимает:
+	// limit - какое количество записей выводить на странице
+	// offset - после какого id выводить записи из базы в количестве limit
+
+	// возвращает:
+	// []types.Place - слайс записей в количестве limit
+	// int - общее количество записей с маркером "places"
+	// или ошибку
+}
+
+func GetPlaces(limit int, offset int) ([]types.Place, int, error) {
+
 }
